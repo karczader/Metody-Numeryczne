3c94a73b8608b08ba6afb37652e540337a9acce2
Karolina Czader <karolina.czader@gmail.com>
2021-03-13 09:43:46 +0100

    poprawka biblioteki

--- NumericalMethods.h
+++ NumericalMethods.h
@@ -1,3 +1,41 @@
-double Langrange(int n, double valuable, double **xy);
+#include<iostream>
+using namespace std;
 
-bool checkIfOkayLangrange(int n, double valuable, double **xy);
+double Langrange(int n, double valuable, double **xy){
+
+    double *l=new double[n];
+    for (int i=0; i<n; i++){
+        l[i]=1.0;
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (j!=i) l[i]*=(valuable-xy[j][0])/(xy[i][0]-xy[j][0]);
+        }
+    }
+
+    double result=0;
+    for (int i=0; i<n; i++){
+        result+=l[i]*xy[i][1];
+    }
+
+    return result;
+}
+
+bool checkIfOkayLangrange(int n, double valuable, double **xy){
+    double max=xy[0][0];
+    double min=xy[0][0];
+    for (int i=0; i<n; i++){
+        if(xy[i][0]<min) min=xy[i][0];
+        if(xy[i][0]>max) max=xy[i][0];
+
+        for (int j=i+1; j<n; j++){
+            //x nie moga sie powtarzac
+            if (xy[i][0]==xy[j][0]) return false;
+        }
+    }
+    //przekroczono przedzial x
+    if (valuable<min || valuable> max) return false;
+    
+    return true;
+
+} 
